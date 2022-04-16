54b04e1652b95d563e07bd4ae2a73bbe5a046d68
Ohio2 <sekmartyna4@gmail.com>
2022-04-16 17:28:09 +0200

    whatever

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=14" };
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -40,8 +40,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	//{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	//{ "Firefox",  NULL,       NULL,       ,       0,           -1 },
+	{ NULL, NULL, NULL, NULL, NULL}
 };
 
 /* layout(s) */
@@ -61,9 +62,6 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define ALT Mod1Mask
 #define PRTSCR	    0x0000ff61
-#define F1 67
-#define F2 68
-#define F3 69
 /* Tag Keys */
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
@@ -81,14 +79,16 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browser[] = { "firefox", NULL };
-static const char *flameshot[] = { "flameshot", "gui", NULL };
-static const char *upvol[]   = { "/usr/bin/pamixer", "--allow-boost", "-i", "5",  NULL };
-static const char *downvol[] = { "/usr/bin/pamixer", "--allow-boost", "-d", "5",  NULL };
-static const char *mutevol[] = { "/usr/bin/pamixer", "-t",  NULL };
-static const char *resetvol[] = { "/usr/bin/pamixer", "--set-volume", "100", NULL };
+//static const char *flameshot[] = { "flameshot", "gui", NULL };
+static const char *upvol[]   = { "/usr/bin/pamixer", "--sink", "alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1", "--allow-boost","-i", "5",  NULL };
+static const char *downvol[] = { "/usr/bin/pamixer", "--sink", "alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1", "--allow-boost", "-d", "5",  NULL };
+static const char *mutevol[] = { "/usr/bin/pamixer", "--sink", "alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1", "-t",  NULL };
+static const char *resetvol[] = { "/usr/bin/pamixer", "--sink", " alsa_output.pci-0000_01_00.1.hdmi-stereo-extra1", "--set-volume", "100", NULL };
 static const char *xkbmapukr[] = { "/usr/bin/setxkbmap", "ru" };
 static const char *xkbmappol[] = { "/usr/bin/setxkbmap", "pl" };
 static const char *poweroff[] = { "/usr/bin/sudo", "poweroff" };
+static const char *screenshotsel[] = {"sh", "-c", "$HOME/.config/dwm/prtscr.sh", "sel", NULL};
+static const char *screenshotfull[] = {"sh", "-c", "$HOME/.config/dwm/prtscr.sh", "full", NULL};
 
 /* keys */
 static Key keys[] = {
@@ -99,7 +99,8 @@ static Key keys[] = {
 	{ ControlMask,                  XK_F4,     spawn,          {.v = resetvol}},
         { ControlMask,                  XK_F5,     spawn,          {.v = xkbmapukr}},
 	{ ControlMask,                  XK_F6,     spawn,          {.v = xkbmappol}},
-	{ 0,     			PRTSCR,    spawn,          {.v = flameshot}},
+	{ 0,     			PRTSCR,    spawn,          SHCMD("$HOME/.config/dwm/prtscr.sh sel")},
+	{ ShiftMask,			PRTSCR,    spawn,          SHCMD("$HOME/.config/dwm/prtscr.sh full")},
 	{ ALT,                          XK_b,      spawn,          {.v = browser } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },	
@@ -131,7 +132,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
-	{ ControlMask|ALT,              XK_Delete, spawn,          {.v = poweroff}},
+	{ ControlMask|ALT,              XK_Escape, spawn,          {.v = poweroff}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
